192bcfbc3ee895ce394b28795f0907cb9a07f59f
Kulaseril, Annette Matthew <u1139839@internal.imsglobal.com>
2025-01-05 05:46:04 +0530

    Data Ingestion

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from src.textSummarizer.logging import logger
+from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Welcome")
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try: 
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX============X")
+except Exception as e:
+    logger.exception(e)
+    raise e
