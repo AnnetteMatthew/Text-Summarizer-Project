aab834064ad93acc6bf52c2bfa524136352ad3eb
Kulaseril, Annette Matthew <u1139839@internal.imsglobal.com>
2025-01-07 23:31:26 +0530

    model_training

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.textSummarizer.logging import logger
 from src.textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from src.textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from src.textSummarizer.pipeline.stage_04_model_trainer import ModelTrainerPipeline
 
 
 # STAGE_NAME = "Data Ingestion Stage"
@@ -14,12 +16,34 @@ from src.textSummarizer.pipeline.stage_02_data_validation import DataValidationT
 #     logger.exception(e)
 #     raise e
 
-STAGE_NAME = "Data Validation Stage"
+# STAGE_NAME = "Data Validation Stage"
+
+# try: 
+#     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+#     data_validation = DataValidationTrainingPipeline()
+#     data_validation.main()
+#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX============X")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+# STAGE_NAME = "Data Transformation Stage"
+
+# try: 
+#     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+#     data_transformation = DataTransformationTrainingPipeline()
+#     data_transformation.main()
+#     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX============X")
+# except Exception as e:
+#     logger.exception(e)
+#     raise e
+
+STAGE_NAME = "Model Training Stage"
 
 try: 
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
-    data_validation = DataValidationTrainingPipeline()
-    data_validation.main()
+    model_trainer = ModelTrainerPipeline()
+    model_trainer.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nX============X")
 except Exception as e:
     logger.exception(e)
